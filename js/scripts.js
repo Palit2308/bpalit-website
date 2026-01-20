6f214587a2879fafb48d7f154233acb25ae91f4d
Palit2308 <biswajitpalit23.08.01@gmail.com>
2026-01-20 22:44:30 +0100

    updating visualisation in smaller screens 3.0

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,23 @@
   const openBtn = document.querySelector('.chat-open');
   const closeBtn = document.querySelector('.chat-close');
   const chatWindow = document.querySelector('.chat-window');
+  const chatMin = document.querySelector('.chat-min');
 
+  // Open from teaser card
   openBtn.addEventListener('click', () => {
     chatWindow.hidden = false;
+    chatMin.hidden = true;
   });
 
+  // Close (minimize) from X
   closeBtn.addEventListener('click', () => {
     chatWindow.hidden = true;
+    chatMin.hidden = false;
+  });
+
+  // Reopen from minimized "A"
+  chatMin.addEventListener('click', () => {
+    chatWindow.hidden = false;
+    chatMin.hidden = true;
   });
 </script>
